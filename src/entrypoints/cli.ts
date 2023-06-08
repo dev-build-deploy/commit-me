9121b6e591f022993f954fca7085910daec75237
Kevin de Jong <monkaii@hotmail.com>
2023-06-08 11:43:30 +0200

    feat: handle exit codes according to validation results
    The following behavior is added:
    - CLI: returns non-zero in case _at least_ 1 validation error has been found
    - GHA: fails the execution in case _at least_ 1 validation error has been found

--- src/entrypoints/cli.ts
+++ src/entrypoints/cli.ts
@@ -8,7 +8,7 @@ SPDX-License-Identifier: GPL-3.0-or-later
 
 import { Command } from "commander";
 import { GitSource } from "../datasources";
-import { RequirementError, parse } from "../conventional_commit";
+import { validate } from "../validator";
 
 const program = new Command();
 
@@ -27,25 +27,28 @@ program
     console.log("📄 CommitMe - Conventional Commit compliance validation");
     console.log("-------------------------------------------------------");
 
-    const errors: string[] = [];
     const datasource = new GitSource();
+    const commits = await datasource.getCommitMessages();
 
-    try {
-      const commits = await datasource.getCommitMessages();
-      commits.forEach(commit => {
-        try {
-          parse(commit);
-        } catch (error) {
-          if (Array.isArray(error)) {
-            error.filter(e => e instanceof RequirementError).forEach(e => errors.push(e.message));
-          }
-        }
-      });
-    } catch (error) {
-      console.log(error);
+    let errorCount = 0;
+    const results = validate(commits);
+
+    for (const commit of results) {
+      console.log(
+        `${commit.errors.length === 0 ? "✅" : "❌"} ${commit.commit.hash}: ${commit.commit.message.substring(0, 77)}${
+          commit.commit.message.length > 80 ? "..." : ""
+        }`
+      );
+      commit.errors.forEach(error => console.log(error));
+      errorCount += commit.errors.length;
     }
 
-    console.log(errors.join("\n"));
+    console.log("-------------------------------------------------------");
+    if (errorCount === 0) {
+      console.log(`✅ All your commits are compliant with Conventional Commit.`);
+    } else {
+      program.error(`❌ Found ${errorCount} Conventional Commit compliance issues.`);
+    }
   });
 
 program.parse(process.argv);
