51e3ed928200fa98f78183c75f827f83c4dfad38
Kevin de Jong <monkaii@hotmail.com>
2023-06-08 10:39:47 +0200

    feat: basic Pull Request validation in GHA
    This commit introduces a very basic validation of Conventional
    Commit messages based on the commits in your current pull request.
    
    As of now, this requires:
    - A GitHub Token with `pull-requests` read access
    - Execution as part of the `pull_requests" trigger
    
    Failing to comply to the above will lead to runtime errors.
    Also; any fixup and merge commit will result in failures.

--- src/entrypoints/action.ts
+++ src/entrypoints/action.ts
@@ -5,6 +5,9 @@ SPDX-License-Identifier: GPL-3.0-or-later
 */
 
 import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { GitHubSource } from "../datasources";
+import { RequirementError, parse } from "../conventional_commit";
 
 /**
  * Main entry point for the GitHub Action.
@@ -12,6 +15,33 @@ import * as core from "@actions/core";
 async function run(): Promise<void> {
   try {
     core.info("📄 CommitMe - Conventional Commit compliance validation");
+    core.info("-------------------------------------------------------");
+
+    const token = core.getInput("token");
+    const octokit = github.getOctokit(token);
+    const datasource = new GitHubSource(octokit);
+    const commits = await datasource.getCommitMessages();
+
+    const errors: string[] = [];
+    try {
+      commits.forEach(commit => {
+        try {
+          parse(commit);
+          core.info(`✅ ${commit.hash}`);
+        } catch (error) {
+          core.startGroup(`❌ ${commit.hash}`)
+          if (Array.isArray(error)) {
+            error.filter(e => e instanceof RequirementError).forEach(e => {
+              core.error(`❌ ${e.message}`)
+              errors.push(e.message)
+            });
+          }
+          core.endGroup();
+        }
+      });
+    } catch (error) {
+      core.error(`${error}`);
+    }
   } catch (ex) {
     core.setFailed((ex as Error).message);
   }
